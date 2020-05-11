2a264399c37514c6d830a3de8f72a074bd565128
unknown <vladstarcev@yandex.ru>
2020-05-11 10:23:37 +0300

    initial set up: added server with express, first get route, changed folder paths according to express

---
+++ app.js
@@ -0,0 +1,34 @@
+//REQUIRING PACKAGES AND EXPRESS BASIC SET UP
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+//VARIABLES AND DUMMY DB
+
+
+
+
+
+
+//GET REQUESTS HANDLING
+app.get("/", function(req,res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+
+//POST REQUESTS HANDLING
+
+
+
+
+
+
+//SERVER SET UP ON PORT 3000
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
