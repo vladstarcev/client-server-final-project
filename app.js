21127635a2df297caed12d80440eafc592117c02
unknown <vladstarcev@yandex.ru>
2020-05-21 01:46:33 +0300

    added api call for USD ILS exchange rate

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const ejs = require('ejs');
+const https = require('https');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'));
 
 //READING DATA FROM JSON
@@ -16,7 +19,7 @@ let data = JSON.parse(rawdata);
 
 //VARIABLES AND DUMMY DB
 var transactions = [];
-var purchases = [0,0,0,0]
+var purchases = [0, 0, 0, 0];
 
 
 
@@ -29,28 +32,28 @@ var purchases = [0,0,0,0]
 
 
 //GET REQUESTS HANDLING
-app.get("/", function(req,res) {
+app.get("/", function(req, res) {
   res.render("main", {
     cellphones: data,
     purchases: purchases
   });
 });
 
-app.get("/buyPc", function(req,res) {
+app.get("/buyPc", function(req, res) {
   res.render("buyPc", {
     cellphones: data,
     purchases: purchases
   });
 });
 
-app.get("/buyCellphone", function(req,res) {
+app.get("/buyCellphone", function(req, res) {
   res.render("cellphones", {
     cellphones: data,
     purchases: purchases
   });
 });
 
-app.get("/pconfirm", function(req,res) {
+app.get("/pconfirm", function(req, res) {
   res.render("pconfirm", {
     cellphones: data,
     purchases: purchases
@@ -58,8 +61,8 @@ app.get("/pconfirm", function(req,res) {
 });
 
 //POST REQUESTS HANDLING
-app.post("/buyCellphone", function(req,res){
-  var transaction = {
+app.post("/buyCellphone", function(req, res) {
+  var userInput = {
     phone: req.body.phone,
     model: req.body.model,
     price: data[req.body.phone].models[req.body.model].price,
@@ -69,42 +72,46 @@ app.post("/buyCellphone", function(req,res){
     year: req.body.card_year,
     cvv: req.body.card_cvv
   };
+  console.log(userInput);
+  var date = new Date();
+  var dateString = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
 
-  transactions.push(makeTransaction(transaction));
-  purchases[req.body.phone]++;
+  const url = "https://api.exchangeratesapi.io/latest?base=USD&symbols=ILS";
+  https.get(url, function(response) {
+    response.on("data", function(d) {
+      const rate = JSON.parse(d);
+      const exchangeRate = rate.rates.ILS;
+      var lp = Number(userInput.price.slice(0, userInput.price.length - 1)) * exchangeRate;
+      var tp = lp + lp * 17 / 100;
+      console.log(exchangeRate, lp, tp);
+      var t = {
+        id: (transactions.length + 1),
+        product: data[userInput.phone].id,
+        model: data[userInput.phone].models[userInput.model].type,
+        date: dateString,
+        user: "Valerie Luna",
+        price: userInput.price,
+        local_price: lp + "ILS",
+        total_price: tp + "ILS",
+        number: userInput.number,
+        name: userInput.name,
+        exp_date: userInput.month + '/' + userInput.year,
+        cvv: userInput.cvv,
+        memo: ""
+      };
 
-  console.log(transactions);
-  console.log(purchases);
-  res.redirect("pconfirm");
+      transactions.push(t);
+      purchases[req.body.phone]++;
 
+      console.log(transactions);
+      console.log(purchases);
+      res.redirect("pconfirm");
+    });
+  });
 });
 
 //FUNCTIONS
-function makeTransaction(info) {
-  var date = new Date();
-  var dateString = date.getDate()+"/"+(date.getMonth() + 1)+"/"+date.getFullYear();
 
-  var lp = Number(info.price.slice(0, info.price.length-1))*3.5; //making a local price (should add an API converter)
-  var tp = lp + lp*17/100; //making a total price
-
-  var t = {
-    id: (transactions.length + 1),
-    product: data[info.phone].id,
-    model: data[info.phone].models[info.model].type,
-    date: dateString,
-    user: "Valerie Luna",
-    price: info.price,
-    local_price: lp + "ILS",
-    total_price: tp + "ILS",
-    number: info.number,
-    name: info.name,
-    exp_date: info.month + '/' + info.year,
-    cvv: info.cvv,
-    memo: ""
-  }
-
-  return t;
-};
 
 
 
